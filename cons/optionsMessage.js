6aad32ad99225dfac60f12e4de889a80b969a7f5
daniel <daniel.lopez@escotel.com.mx>
2025-03-04 13:18:54 -0600

    con búsqueda de contactos por nombre y/o telefono

--- cons/optionsMessage.js
+++ cons/optionsMessage.js
@@ -6,20 +6,6 @@ const authFacebook = {
     "Authorization": `Bearer ${process.env.WHATSAPP_API_KEY}`
   }};
 
-const optionsMessage =(data) => {
-  const options ={
-  host: 'graph.facebook.com',
-  path: '/v21.0/451082418098784/messages',
-  method: 'POST',
-  body: data,
-  headers: {
-    "Content-type": "application/json",
-    "Authorization": `Bearer ${process.env.WHATSAPP_API_KEY}`
-  }}
-  return options;
-};
-
 module.exports ={
   authFacebook,
-  optionsMessage,
 };
