7ab69be6cf8a1e4fe0c6f42f0de2edb90628956d
daniel <daniel.lopez@escotel.com.mx>
2025-03-04 13:27:56 -0600

    con búsqueda de contactos por nombre y/o telefono

--- cons/urls.js
+++ cons/urls.js
@@ -1,5 +1,5 @@
-const urlMeta = "https://graph.facebook.com/v21.0/451082418098784"; //pruebas
-// const urlMeta = "https://graph.facebook.com/v21.0/484978648036027"; //produccion
+const urlMeta = process.env.URL_META;
+
 
 module.exports ={
   urlMeta,
