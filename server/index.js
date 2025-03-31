8661cc708cca1c26757a43759095daaa72e7295f
daniel <daniel.lopez@escotel.com.mx>
2025-03-31 13:48:40 -0600

    proyecto de pruebas con netlify

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors:{
-    origin: ['https://wa.altaespecialidad.escotel.mx', 'https://jovial-malasada-025646.netlify.app'],
+    origin: ['https://dev-wa.asignacion.escotel.mx', 'https://jovial-malasada-025646.netlify.app'],
     credentials:true
   }
 });
